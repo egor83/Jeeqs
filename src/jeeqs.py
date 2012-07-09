cbedd20a624239b0409d55ce9d3fe0fc0093fd7d
nomadali <a.akhavan.b@gmail.com>
2012-07-08 23:46:37 -0700

    Towards #78

--- src/jeeqs.py
+++ src/jeeqs.py
@@ -35,10 +35,50 @@ jinja_environment = jinja2.Environment(
 jinja_environment.filters['escapejs'] = escapejs
 jinja_environment.filters['timesince'] = timesince
 
+## oauth2
+###############
+
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.appengine import oauth2decorator_from_clientsecrets
+from google.appengine.api import memcache
 
 # Set to True if stack traces should be shown in the browser, etc.
 # TODO: should this be changed into an environment variable ?
 _DEBUG = True
+
+# CLIENT_SECRETS, name of a file containing the OAuth 2.0 information for this
+# application, including client_id and client_secret, which are found
+# on the API Access tab on the Google APIs
+# Console <http://code.google.com/apis/console>
+CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
+
+# Helpful message to display in the browser if the CLIENT_SECRETS file
+# is missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+<h1>Warning: Please configure OAuth 2.0</h1>
+<p>
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+</p>
+<p>
+<code>%s</code>.
+</p>
+<p>with information found on the <a
+href="https://code.google.com/apis/console">APIs Console</a>.
+</p>
+""" % CLIENT_SECRETS
+
+http = httplib2.Http(memcache)
+service = build("plus", "v1", http=http)
+decorator = oauth2decorator_from_clientsecrets(
+    CLIENT_SECRETS,
+    'https://www.googleapis.com/auth/plus.me',
+    MISSING_CLIENT_SECRETS_MESSAGE)
+
+###############
+## end of oauth2
 
 def get_jeeqs_robot():
     """
@@ -214,6 +254,7 @@ class AboutHandler(webapp2.RequestHandler):
     """Renders the About page """
 
     @authenticate(required=False)
+    @decorator.oauth_required
     def get(self):
         vars = add_common_vars({
                 'jeeqser' : self.jeeqser,
