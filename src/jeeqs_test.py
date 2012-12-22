f63c042a8fc425c1810b2fde6cf0e83bac9e3418
nomadali <nomadali@google.com>
2012-12-21 21:18:00 -0500

    Added test runner

--- src/jeeqs_test.py
+++ src/jeeqs_test.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
 import mox
+import optparse
 import program_handler
+import sys
 import traceback
 import unittest
+import unittest2
 import webapp2
 import webtest
 
@@ -79,5 +83,30 @@ print 1
     self.assertTrue('1' in response.body)
     self.assertTrue('200' in response.status)
 
+USAGE = """%prog SDK_PATH TEST_PATH
+Run unit tests for App Engine apps.
+
+SDK_PATH    Path to the SDK installation
+TEST_PATH   Path to package containing test modules"""
+
+
+def main(sdk_path, test_path):
+  sys.path.insert(0, sdk_path)
+  import dev_appserver
+  dev_appserver.fix_sys_path()
+  suite = unittest2.loader.TestLoader().discover(test_path, pattern="*_test.py")
+  unittest2.TextTestRunner(verbosity=2).run(suite)
+
+
 if __name__ == '__main__':
-  unittest.main()
+  #  unittest.main()
+  parser = optparse.OptionParser(USAGE)
+  options, args = parser.parse_args()
+  if len(args) != 2:
+    print 'Error: Exactly 2 arguments required.'
+    parser.print_help()
+    sys.exit(1)
+  SDK_PATH = args[0]
+  TEST_PATH = args[1]
+  main(SDK_PATH, TEST_PATH)
+
