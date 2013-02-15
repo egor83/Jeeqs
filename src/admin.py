c1c7b36286f40c6777edde352864331de4cb0f9f
nomadali <nomadali@google.com>
2013-02-14 19:23:25 -0500

    Removing django forms as it doesn't support ndb.

--- src/admin.py
+++ src/admin.py
@@ -5,44 +5,8 @@ from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from google.appengine.ext.db import djangoforms
-
 import core
 from models import *
-
-
-class ChallengeForm(djangoforms.ModelForm):
-    class Meta:
-        model = Challenge
-
-
-class ChallengeDjangoFormPage(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write(unicode('<html><body>'
-                                '<form method="POST" '
-                                'action="/admin/challenges/new">'
-                                '<table>'))
-        self.response.out.write(unicode(ChallengeForm()))
-        self.response.out.write(unicode('</table>'
-                                '<input type="submit">'
-                                '</form></body></html>'))
-
-    def post(self):
-        data = ChallengeForm(data=self.request.POST)
-        if data.is_valid():
-            entity = data.save(commit=True)
-            entity.added_by = users.get_current_user()
-            entity.put()
-            self.redirect('/admin/challenges')
-        else:
-            self.response.out.write('<html><body>'
-                                    '<form method="POST" '
-                                    'action="/">'
-                                    '<table>')
-            self.response.out.write(data)
-            self.response.out.write('</table>'
-                                    '<input type="submit">'
-                                    '</form></body></html>')
 
 
 class ChallengePage(webapp.RequestHandler):
@@ -51,7 +15,7 @@ class ChallengePage(webapp.RequestHandler):
     """
     @core.authenticate(required=True)
     def get(self):
-        all_courses = Course.all().fetch(1000)
+        all_courses = Course.query().fetch(1000)
 
         vars = core.add_common_vars({
             'courses': all_courses,
@@ -105,48 +69,11 @@ class ChallengeListPage(webapp.RequestHandler):
             '<br/><a href="/admin/challenges/new">New Challenge</a>')
 
 
-class ChallengeEditPage(webapp.RequestHandler):
-    def get(self):
-        key = self.request.get('key')
-        challenge = Challenge.get(key)
-        self.response.out.write(unicode('<html><body>'
-                                '<form method="POST" '
-                                'action="/admin/challenges/edit">'
-                                '<table>'))
-        self.response.out.write(unicode(ChallengeForm(instance=challenge)))
-        self.response.out.write(unicode('</table>'
-                                '<input type="hidden" name="key" value="%s">'
-                                '<input type="submit">'
-                                '</form></body></html>' % key))
-
-    def post(self):
-        key = self.request.get('key')
-        challenge = Challenge.get(key)
-        data = ChallengeForm(data=self.request.POST, instance=challenge)
-        if data.is_valid():
-            entity = data.save(commit=True)
-            entity.put()
-            self.redirect('/admin/challenges')
-        else:
-            self.response.out.write(unicode('<html><body>'
-                                    '<form method="POST" '
-                                    'action="/admin/challenges/edit">'
-                                    '<table>'))
-            self.response.out.write(unicode(data))
-            self.response.out.write(unicode('</table>'
-                                    '<input type="hidden" name="key"'
-                                    ' value="%s">'
-                                    '<input type="submit">'
-                                    '</form></body></html>' % key))
-
-
 def main():
     application = webapp.WSGIApplication(
         [('/admin/challenges/new', ChallengePage),
-            ('/admin/challenges/new_django', ChallengeDjangoFormPage),
             ('/admin/challenges', ChallengeListPage),
             ('/admin/challenges/', ChallengeListPage),
-            ('/admin/challenges/edit', ChallengeEditPage)
          ],
         debug=True)
     run_wsgi_app(application)
