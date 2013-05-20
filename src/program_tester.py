88e5f34e6d9fbcb75443b92fbc7212c66e13e9fa
Egor Ryabkov <egor.ryabkov@gmail.com>
2013-05-20 08:02:02 +0400

    Reacting to code review for #226
    pep8 stuff, whitespace changes only.

--- src/program_tester.py
+++ src/program_tester.py
@@ -17,99 +17,99 @@ from models import *
 
 
 def compile_and_run(program):
-  """
-  Compiles and runs the given program.
-  :param program: program to compile and run.
-  :return: stdout, stderr,
-  """
+    """
+    Compiles and runs the given program.
+    :param program: program to compile and run.
+    :return: stdout, stderr,
+    """
 
-  output = ""
-  # the python compiler doesn't like network line endings.
-  program = program.replace('\r\n', '\n')
-  # add a couple newlines at the end of the program. this makes
-  # single-line expressions such as 'class Foo: pass' evaluate happily.
-  program += '\n\n'
-  try:
-      compiled = compile(program, '<sudmitted>', 'exec')
-  except:
-      return 'Compile error:  \n' + format_exc(), None
-  program_module = new.module('__main__')
-  old_main = sys.modules.get('__main__')
-  #TODO(majid) Does this properly sandbox the builtins. I can call print!
-  #print program_module.__dict__
-  try:
-    sys.modules['__main__'] = program_module
-    program_module.__name__ = '__main__'
+    output = ""
+    # the python compiler doesn't like network line endings.
+    program = program.replace('\r\n', '\n')
+    # add a couple newlines at the end of the program. this makes
+    # single-line expressions such as 'class Foo: pass' evaluate happily.
+    program += '\n\n'
     try:
-        stdout_buffer = StringIO.StringIO()
-        stderr_buffer = StringIO.StringIO()
-        old_stdout = sys.stdout
-        old_stderr = sys.stderr
-        try:
-            sys.stdout = stdout_buffer
-            sys.stderr = stderr_buffer
-            # allowed imports
-            import math
-            builtins = {}
-            for item in dir(math):
-                if hasattr(math.__dict__.get(item), '__call__'):
-                    builtins[item] = math.__dict__.get(item)
-            program_module.__builtins__ = builtins
-            exec compiled in program_module.__dict__
-            #exec compiled in {'__builtins__': builtins}, {}
-        finally:
-            sys.stdout = old_stdout
-            sys.stderr = old_stderr
-            output += stdout_buffer.getvalue()
-            output += stderr_buffer.getvalue()
+        compiled = compile(program, '<sudmitted>', 'exec')
     except:
-        return 'Execution error: \n' + format_exc(), None
-    return output, program_module
-  finally:
-      sys.modules['__main__'] = old_main
+        return 'Compile error:  \n' + format_exc(), None
+    program_module = new.module('__main__')
+    old_main = sys.modules.get('__main__')
+    #TODO(majid) Does this properly sandbox the builtins. I can call print!
+    #print program_module.__dict__
+    try:
+        sys.modules['__main__'] = program_module
+        program_module.__name__ = '__main__'
+        try:
+            stdout_buffer = StringIO.StringIO()
+            stderr_buffer = StringIO.StringIO()
+            old_stdout = sys.stdout
+            old_stderr = sys.stderr
+            try:
+                sys.stdout = stdout_buffer
+                sys.stderr = stderr_buffer
+                # allowed imports
+                import math
+                builtins = {}
+                for item in dir(math):
+                    if hasattr(math.__dict__.get(item), '__call__'):
+                        builtins[item] = math.__dict__.get(item)
+                program_module.__builtins__ = builtins
+                exec compiled in program_module.__dict__
+                #exec compiled in {'__builtins__': builtins}, {}
+            finally:
+                sys.stdout = old_stdout
+                sys.stderr = old_stderr
+                output += stdout_buffer.getvalue()
+                output += stderr_buffer.getvalue()
+        except:
+            return 'Execution error: \n' + format_exc(), None
+        return output, program_module
+    finally:
+        sys.modules['__main__'] = old_main
 
 
 def format_exc():
-  etype, value, tb = sys.exc_info()
-  #ignore the first frame which is this frame!
-  t = ''.join(traceback.format_exception(etype, value, tb)[2:])
-  return t  # .replace('\n', '  \n').replace(' ', '&nbsp;')
+    etype, value, tb = sys.exc_info()
+    #ignore the first frame which is this frame!
+    t = ''.join(traceback.format_exception(etype, value, tb)[2:])
+    return t  # .replace('\n', '  \n').replace(' ', '&nbsp;')
 
 
 def run_testcases(program, challenge):
-  """
-  Runs the challenge's test cases over the given program.
+    """
+    Runs the challenge's test cases over the given program.
 
-  :param program: program to test
-  :param challenge: challenge for which the program was submitted
-  :return: A review value of correct/incorrect and the output of the test
-  executions.
-  """
-  success = True
-  review = Review.CORRECT
-  output, program_module = compile_and_run(program)
-  if not program_module:
-    success = False
-    review = Review.INCORRECT
-  else:
-    test_num = 0
-    for test in challenge.testcases:
-        test_num = test_num + 1
-        result = eval(test.statement, program_module.__dict__)
-        if not str(result) == test.expected:
-            success = False
-            output += " Failed with the statement:  \n *****  \n" \
-                + test.statement \
-                + '  \n Expected result:  \n' \
-                + test.expected \
-                + '  \n Actual result:  \n' \
-                + str(result) \
-                + '   \n'
-    if test_num == 0:
-      output += 'No test cases to run!'
-    elif success:
-      output = 'Success! All tests ran successfully!' + output
+    :param program: program to test
+    :param challenge: challenge for which the program was submitted
+    :return: A review value of correct/incorrect and the output of the test
+    executions.
+    """
+    success = True
+    review = Review.CORRECT
+    output, program_module = compile_and_run(program)
+    if not program_module:
+        success = False
+        review = Review.INCORRECT
     else:
-      output = 'At least one of the test cases failed: ' + output
-      review = Review.INCORRECT
-  return review, output
+        test_num = 0
+        for test in challenge.testcases:
+            test_num = test_num + 1
+            result = eval(test.statement, program_module.__dict__)
+            if not str(result) == test.expected:
+                success = False
+                output += " Failed with the statement:  \n *****  \n" \
+                    + test.statement \
+                    + '  \n Expected result:  \n' \
+                    + test.expected \
+                    + '  \n Actual result:  \n' \
+                    + str(result) \
+                    + '   \n'
+        if test_num == 0:
+            output += 'No test cases to run!'
+        elif success:
+            output = 'Success! All tests ran successfully!' + output
+        else:
+            output = 'At least one of the test cases failed: ' + output
+            review = Review.INCORRECT
+    return review, output
--- src/test_program_tester.py
+++ src/test_program_tester.py
@@ -8,47 +8,47 @@ __author__ = 'nomadali'
 
 
 class ProgramTesterTestCase(test_jeeqs.JeeqsTestCase):
-  def setUp(self):
-    test_jeeqs.JeeqsTestCase.setUp(self)
+    def setUp(self):
+        test_jeeqs.JeeqsTestCase.setUp(self)
 
-  def tearDown(self):
-    test_jeeqs.JeeqsTestCase.tearDown(self)
+    def tearDown(self):
+        test_jeeqs.JeeqsTestCase.tearDown(self)
 
-  def test_compile_and_run_pass(self):
-    """test compile_and_run "pass"."""
-    program = 'pass'
-    output, program_module = program_tester.compile_and_run(program)
-    self.assertEquals(output, "")
-    self.assertIsNotNone(program_module)
+    def test_compile_and_run_pass(self):
+        """test compile_and_run "pass"."""
+        program = 'pass'
+        output, program_module = program_tester.compile_and_run(program)
+        self.assertEquals(output, "")
+        self.assertIsNotNone(program_module)
 
-  def test_compile_and_run_compile_error(self):
-    """test compile_and_run "pass"."""
-    program = 'error!'
-    output, program_module = program_tester.compile_and_run(program)
-    self.assertIsNone(program_module)
-    self.assertTrue('Compile error' in output)
+    def test_compile_and_run_compile_error(self):
+        """test compile_and_run "pass"."""
+        program = 'error!'
+        output, program_module = program_tester.compile_and_run(program)
+        self.assertIsNone(program_module)
+        self.assertTrue('Compile error' in output)
 
-  def test_compile_and_run_factorial(self):
-    program = '''
+    def test_compile_and_run_factorial(self):
+        program = '''
 def factorial(n):
-  return 1 if n == 1 else n * factorial(n-1)
+    return 1 if n == 1 else n * factorial(n-1)
 
-    '''
-    output, program_module = program_tester.compile_and_run(program)
-    self.assertEquals(output, "")
-    self.assertIsNotNone(program_module)
+        '''
+        output, program_module = program_tester.compile_and_run(program)
+        self.assertEquals(output, "")
+        self.assertIsNotNone(program_module)
 
-  def test_testcases_factorial(self):
-    program = """
+    def test_testcases_factorial(self):
+        program = """
 def factorial(n):
-  return 1 if n == 1 else n * factorial(n-1)
-    """
-    challenge = self.CreateChallenge(name_persistent="Factorial")
-    TestCase(
-        challenge=challenge.key,
-        statement="factorial(3)",
-        expected="6").put()
-    challenge = Challenge.query().filter(
-        Challenge.name_persistent == 'Factorial').fetch(1)[0]
-    review, output = program_tester.run_testcases(program, challenge)
-    self.assertEquals(review, Review.CORRECT)
+    return 1 if n == 1 else n * factorial(n-1)
+        """
+        challenge = self.CreateChallenge(name_persistent="Factorial")
+        TestCase(
+            challenge=challenge.key,
+            statement="factorial(3)",
+            expected="6").put()
+        challenge = Challenge.query().filter(
+            Challenge.name_persistent == 'Factorial').fetch(1)[0]
+        review, output = program_tester.run_testcases(program, challenge)
+        self.assertEquals(review, Review.CORRECT)
